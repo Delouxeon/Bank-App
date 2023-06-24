e53de4e8792b663ea4b500a8a78440e453ca1549
delouxeon <oluxnwabuife@gmail.com>
2023-06-24 13:40:04 +0100

    files update

--- src/components/CreateAcc/CreateAccount.js
+++ src/components/CreateAcc/CreateAccount.js
@@ -4,52 +4,81 @@ import greenHandEmoji from '../../assets/greenHandEmoji.png';
 
 function CreateAccount() {
   return (
-    <div className ="createAccountContainer">
+    <div className="createAccountContainer">
       <header>
-        <img src = {SaverbankLogo} alt='Saver Bank Logo'/>
+        <img src={SaverbankLogo} alt="Saver Bank Logo" />
       </header>
-      <form action="">
-        <div className="form-header">
-          <h1 className="form-heading">Create an Account</h1>
-          <span>
-            <img src={greenHandEmoji} alt="green hand emoji" />
-          </span>
-          <p className="form-header-p">Please enter required information</p>
-        </div>
-        <div className="inputs-wrapper">
-          <div className="first-name">
-            <label htmlFor="fname">First Name</label> <br />
-            <input type="text" />
+      <section></section>
+      <section className="forms-wrapper">
+        <form id="logIn-form" className="logIn-form" action="">
+          <div className="form-header">
+            <h1 className="form-heading">Login</h1>
           </div>
-          <div className="last-name">
-            <label htmlFor="lname">Last Name</label> <br />
-            <input type="text" />
+          <div className="inputs-wrapper">
+            <div className="email">
+              <label htmlFor="email">Email Address</label> <br />
+              <input type="email" />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label> <br />
+              <input type="password" />
+            </div>
+            <div className="logIn-btn-div">
+              <input className="btn logIn-btn" type="submit" value="Log In" />
+            </div>
+            <div className="sign-up-call">
+              <p>
+                Don't have an account? <a href="">Sign Up</a>
+              </p>
+            </div>
           </div>
-          <div className="email">
-            <label htmlFor="email">Email Address</label> <br />
-            <input type="email" />
+        </form>
+        <form className="signUp-form" action="">
+          <div className="form-header">
+            <h1 className="form-heading">Create an Account</h1>
+            <span>
+              <img src={greenHandEmoji} alt="green hand emoji" />
+            </span>
+            <p className="form-header-p">Please enter required information</p>
           </div>
-          <div className="phone-no">
-            <label htmlFor="phone-no">Phone Number</label> <br />
-            <input type="tel" />
+          <div className="inputs-wrapper">
+            <div className="firstName-div">
+              <label htmlFor="fname">First Name</label> <br />
+              <input type="text" />
+            </div>
+            <div className="lastName-div">
+              <label htmlFor="lname">Last Name</label> <br />
+              <input type="text" />
+            </div>
+            <div className="email-div">
+              <label htmlFor="email">Email Address</label> <br />
+              <input type="email" />
+            </div>
+            <div className="phoneNo-div">
+              <label htmlFor="phone-no">Phone Number</label> <br />
+              <input type="tel" />
+            </div>
+            <div className="password-div">
+              <label htmlFor="password">Password</label> <br />
+              <input type="password" />
+            </div>
+            <div className="confirm-password-div">
+              <label htmlFor="confirm-password">Confirm Password</label> <br />
+              <input type="password" />
+            </div>
+            <div className="signUp-btn-div">
+              <input className="btn signUp-btn" type="submit" value="Sign Up" />
+            </div>
+            <div className="log-in-call">
+              <p>
+                Do you have an account? <a href>Login</a>
+              </p>
+            </div>
           </div>
-          <div className="password">
-            <label htmlFor="password">Password</label> <br />
-            <input type="password" />
-          </div>
-          <div className="confirm-password">
-            <label htmlFor="confirm-password">Confirm Password</label> <br />
-            <input type="password" />
-          </div>
-          <div className="signUp-btn">
-            <input className="btn" type="submit" value= "Sign Up" />
-          </div>
-          <div className="log-in-call">
-            <p>Do you have an account? <a href>Login</a></p> 
-          </div>
-        </div>
-      </form>
-      <section className='bg-img-section'></section>
+        </form>
+      </section>
+
+      <section className="bg-img-section"></section>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom client';
+import React from "react";
+//import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 //import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
@@ -14,11 +14,6 @@ root.render(
     <App />
   </StrictMode>
 );
-
-
-
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
