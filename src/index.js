9e628f3190cd49b4083752e1b423cbc9a883f836
delouxeon <oluxnwabuife@gmail.com>
2023-06-24 14:59:50 +0100

    files upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 //import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import "./index.css";
 import App from "./components/App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 //import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
@@ -11,7 +13,9 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
 
