954013d9415dda79d118aac868c4e175a4f05e90
Nwabuife Divine Oluchukwu <88338007+Delouxeon@users.noreply.github.com>
2023-06-24 21:53:59 +0100

    file update

--- src/components/View transactions/Transactions.js
+++ src/components/View transactions/Transactions.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import './Transactions.css'
 
+const Transactions = () => {
+  
+
+  return (
+    <section className="details" >
+    </section>
+  );
+};
+
+export default Transactions;
