e22746762d5026b4a33f59bce6735523b1fb8358
Nwabuife Divine Oluchukwu <88338007+Delouxeon@users.noreply.github.com>
2023-06-24 21:56:04 +0100

    files update

--- src/components/SendAndReceive/SendAndReceive.js
+++ src/components/SendAndReceive/SendAndReceive.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import './SendAndReceive.css'
 
+const SendAndReceive = () => {
+  
+
+  return (
+    <section className="details" >
+    </section>
+  );
+};
+
+export default SendAndReceive;
