3307240d890a198c9e63efe1e511b3912671524e
delouxeon <oluxnwabuife@gmail.com>
2023-06-24 11:50:59 +0100

    files update

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import {StrictMode} from 'react';
-//import {createRoot} from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 //import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+
 
 
 
